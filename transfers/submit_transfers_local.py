71549ee06dffe7425afea20c22538504e178198f
Liis <polaarrebane@gmail.com>
2014-03-04 16:27:43 +0100

    submit data_replica locally

---
+++ transfers/submit_transfers_local.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import re
+import os
+import sys
+sys.path.append('./')
+indir = "TransfersForResubmit/"
+#indir = "Filelists_data/"
+#infilelists_filename = indir + "/filelist_double_el_data.txt"
+
+infilelists_filename = indir + "to_run.txt"
+
+lists = open(infilelists_filename, 'r')
+
+while True: # read line by line
+    infilelist = lists.readline().strip('\n')
+    if not infilelist: break
+    if re.search("skip", infilelist) != None: continue
+
+#    outdir = infilelist.split(".txt")[0]
+#    outdir = (infilelist.split("fileList_")[1]).split(".txt")[0]
+    outdir = (infilelist.split("fail_list_")[1]).split(".txt")[0]
+
+    print "Start processing: " + outdir
+
+
+    os.system('data_replica.py --delete --from-site T2_IT_Pisa --to-site T2_EE_Estonia ' + indir+"/"+ infilelist + ' /store/user/liis/VHbb_patTuples/' + outdir)
+
+
+print "Finished filelists"
