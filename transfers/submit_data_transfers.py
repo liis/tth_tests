c58257870f0fed133247d1ab138f232fb344ac55
Liis <polaarrebane@gmail.com>
2014-03-04 16:25:53 +0100

    submit_data_transfers.py

--- transfers/submit_data_transfers.py
+++ transfers/submit_data_transfers.py
@@ -3,32 +3,38 @@ import re
 import os
 import sys
 sys.path.append('./')
-indir = "VHbb_transfer/from_lorenzo/"
+#indir = "TransfersForResubmit/"
+indir = "Filelists_data_split/"
 #infilelists_filename = indir + "/filelist_double_el_data.txt"
-infilelists_filename = indir + "to_run.txt"
 
+infilelists_filename = indir + "to_run.txt"
 lists = open(infilelists_filename, 'r')
 
 while True: # read line by line
     infilelist = lists.readline().strip('\n')
     if not infilelist: break
     if re.search("skip", infilelist) != None: continue
-    
-    outdir = (infilelist.split("fileList_")[1]).split(".txt")[0]
 
-    print outdir
+    outdir = infilelist.split("_part_")[0]
+#    outdir = infilelist.split(".txt")[0]
+#    outdir = (infilelist.split("fileList_")[1]).split(".txt")[0]
+#    outdir = (infilelist.split("fail_list_")[1]).split(".txt")[0]
+
+    print "Saving output to directory: " + outdir
 
     scriptname = "submit_" + outdir + ".sh"
     f = open(scriptname, 'w')
     f.write('#!/bin/bash\n\n')
     f.write('\n\n')
-    f.write('data_replica.py --from-site T2_IT_Pisa --to-site T2_EE_Estonia ' + indir+"/"+ infilelist + ' /store/user/liis/VHbb_patTuples/' + outdir)
+    f.write('data_replica.py --delete --from-site T2_IT_Pisa --to-site T2_EE_Estonia ' + indir+"/"+ infilelist + ' /store/user/liis/VHbb_patTuples/' + outdir)
     f.write('\n\n')
     f.close()
     os.system('chmod +x ' + scriptname)
 
-    submit = "qsub -V -cwd -q long.q -N copy_" +outdir+ " "+scriptname 
-    print "Submitting..."
+#    submit = "qsub -V -cwd -q long.q -N copy_" +outdir+ " "+scriptname 
+    submit = "qsub -V -cwd -q all.q -N copy_" +outdir+ " "+scriptname 
+    print "Submitting... " + submit
+    
     os.system(submit)
 
 print "Finished job submission"
