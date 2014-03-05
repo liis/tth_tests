29b5ae3d955b21faa0a9c2280a86a070e9f586f4
Liis Rebane <polaarrebane@gmail.com>
2014-03-05 08:00:04 +0200

    submit

--- transfers/submit_data_transfers.py
+++ transfers/submit_data_transfers.py
@@ -10,29 +10,34 @@ indir = "Filelists_data_split/"
 infilelists_filename = indir + "to_run.txt"
 lists = open(infilelists_filename, 'r')
 
+os.system("voms-proxy-init -voms cms")
 while True: # read line by line
     infilelist = lists.readline().strip('\n')
     if not infilelist: break
     if re.search("skip", infilelist) != None: continue
 
     outdir = infilelist.split("_part_")[0]
+    ext = (infilelist.split("_part_")[1]).split(".txt")[0]
 #    outdir = infilelist.split(".txt")[0]
 #    outdir = (infilelist.split("fileList_")[1]).split(".txt")[0]
 #    outdir = (infilelist.split("fail_list_")[1]).split(".txt")[0]
 
     print "Saving output to directory: " + outdir
 
-    scriptname = "submit_" + outdir + ".sh"
+    scriptname = "submit_" + outdir + ext + ".sh"
     f = open(scriptname, 'w')
     f.write('#!/bin/bash\n\n')
     f.write('\n\n')
-    f.write('data_replica.py --delete --from-site T2_IT_Pisa --to-site T2_EE_Estonia ' + indir+"/"+ infilelist + ' /store/user/liis/VHbb_patTuples/' + outdir)
+    #f.write('data_replica.py --delete --from-site T2_IT_Pisa --to-site T2_EE_Estonia ' + indir+"/"+ infilelist + ' /store/user/liis/VHbb_patTuples/' + outdir)
+    
+    f.write('python data_replica.py --delete --from-site T2_IT_Pisa --to-site T2_EE_Estonia ' + indir+"/"+ infilelist + ' /store/user/liis/VHbb_patTuples/' + outdir)
     f.write('\n\n')
     f.close()
     os.system('chmod +x ' + scriptname)
 
 #    submit = "qsub -V -cwd -q long.q -N copy_" +outdir+ " "+scriptname 
-    submit = "qsub -V -cwd -q all.q -N copy_" +outdir+ " "+scriptname 
+#    submit = "qsub -V -cwd -q all.q -N copy_" +outdir+ " "+scriptname
+    submit = "qsub -N copy_" +outdir+ " "+scriptname 
     print "Submitting... " + submit
     
     os.system(submit)
