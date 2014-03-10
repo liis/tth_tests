8b52770644d8070cb78e03f383d3b70c5dbc7737
Liis Rebane <polaarrebane@gmail.com>
2014-03-10 16:17:25 +0200

    all ee

--- transfers/submit_check_transfers.sh
+++ transfers/submit_check_transfers.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-INFILELIST_DIR="VHbb_transfer/from_lorenzo"
-OUTPUT_FILE_DIR="TransfersForResubmit"
-RUN_DATA_REPLICA=0
+INFILELIST_DIR="Filelists_data_el"
+#INFILELIST_DIR="TransfersForResubmit_mc" # where to save output file lists
+OUTPUT_FILE_DIR="TransfersForResubmit_el" # where to save output file lists
+RUN_DATA_REPLICA=0 # whether or not submit data_replica.py to get missing files (otherwise save missing files in file)
 SUBMIT_TO_BATCH=1
 
+
 if [ RUN_DATA_REPLICA == 1 ] ; then
-    PYTHONHOME="/usr/lib/python2.6"
+    PYTHONHOME="/usr/lib/python2.6" #not needed if you avoid cmsenv 
 fi
 
 voms-proxy-init -voms cms
+echo outputdir = $OUTPUT_FILE_DIR
 
 for INFILELIST in `ls -d -1 $INFILELIST_DIR/**` # take whole path
   do
@@ -19,9 +22,9 @@ for INFILELIST in `ls -d -1 $INFILELIST_DIR/**` # take whole path
   if [ $SUBMIT_TO_BATCH == 1 ] ; then
       echo "Submitting to batch"
       chmod +x check_file_transfers.sh
-      qsub check_file_transfers.sh $INFILELIST $RUN_DATA_REPLICA
+      qsub check_file_transfers.sh $INFILELIST $RUN_DATA_REPLICA $OUTPUT_FILE_DIR
   else
-      sh check_file_transfers.sh $INFILELIST $RUN_DATA_REPLICA
+      sh check_file_transfers.sh $INFILELIST $RUN_DATA_REPLICA $OUTPUT_FILE_DIR
   fi
   
 done
